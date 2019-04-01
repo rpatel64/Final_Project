bf4318c96be636aaf22791733a98eb442dc71ea1
Jarod1736 <46686094+Jarod1736@users.noreply.github.com>
2019-04-01 15:57:17 -0400

    connect.php
    code to potentially add user data to database

---
+++ connect.php
@@ -0,0 +1,36 @@
+<?php
+  $email = filter_input(INPUT_POST, 'email');
+  $password = filter_input(INPUT_POST, 'password');
+  if (!empty($email)){
+    if (!empty($password)){
+      $host = "ec2-23-23-92-204.compute-1.amazonaws.com";
+      $dbuser = "gxupvblzzfulmn";
+      $dbpassword = "6f218f9e00cb85e2d96043b8a25898951fd0fbd475a5bcbeb9eb2ba4cc42d072";
+      $dbname = "d1fs1cm170ct9t";
+      // Create connection
+      $conn = new mysqli ($host, $dbuser, $dbpassword, $dbname);
+      if (mysqli_connect_error()){
+        die('Connect Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
+      }
+      else{
+        $sql = "INSERT INTO members (email, password)
+        values ('$email','$password')";
+        if ($conn->query($sql)){
+          echo "New record is inserted sucessfully";
+        }
+        else{
+          echo "Error: ". $sql ."c". $conn->error;
+        }
+      $conn->close();
+    }
+  }
+    else{
+      echo "Password should not be empty";
+      die();
+    }
+  }
+  else{
+   echo "Email should not be empty";
+   die();
+  }
+?>
