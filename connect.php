4e4bc8195e1cc5809be33270b2b62ebb37a8acf3
Jarod1736 <46686094+Jarod1736@users.noreply.github.com>
2019-04-09 19:44:32 -0400

    Update connect.php
    changed the connection to a postgres connection and added user input for testing -- code doesn't work bc of some fatal unable to connect to server error

--- connect.php
+++ connect.php
@@ -1,36 +1,45 @@
 <?php
-$email = filter_input(INPUT_POST, 'email');
-$password = filter_input(INPUT_POST, 'password');
+//$email = filter_input(INPUT_POST, 'email');
+//$password = filter_input(INPUT_POST, 'password');
+echo "please insert your email\n";
+$email = rtrim(fgets(STDIN));
+echo "please insert your password\n";
+$password = rtrim(fgets(STDIN));
+
 if (!empty($email)){
-    if (!empty($password)){
-        $host = "ec2-23-23-92-204.compute-1.amazonaws.com";
-        $dbuser = "gxupvblzzfulmn";
-        $dbpassword = "6f218f9e00cb85e2d96043b8a25898951fd0fbd475a5bcbeb9eb2ba4cc42d072";
-        $dbname = "d1fs1cm170ct9t";
-        // Create connection
-        $conn = new mysqli ($host, $dbuser, $dbpassword, $dbname);
-        if (mysqli_connect_error()){
-            die('Connect Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
-        }
-        else{
-            $sql = "INSERT INTO members (email, password)
-                values ('$email','$password')";
-            if ($conn->query($sql)){
-                echo "New record is inserted sucessfully";
-            }
-            else{
-                echo "Error: ". $sql ."c". $conn->error;
-            }
-            $conn->close();
-        }
+  if (!empty($password)){
+    $host = "ec2-23-23-92-204.compute-1.amazonaws.com";
+    $port = "5432";
+    $dbuser = "gxupvblzzfulmn";
+    $dbpassword = "6f218f9e00cb85e2d96043b8a25898951fd0fbd475a5bcbeb9eb2ba4cc42d072";
+    $dbname = "d1fs1cm170ct9t";
+    // Create connection
+    // $conn = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
+    $conn = pg_connect("host=$host port=$port dbname=$dbname user=$dbuser password=$dbpassword");
+    // if (mysqli_connect_error()){
+    if($conn == false){
+      die('Connection Error');
+      // die('Connect Error ('. /*mysqli_connect_errno()*/ .') '. /*mysqli_connect_error()*/);
     }
     else{
-        echo "Password should not be empty";
-        die();
+      $sql = "INSERT INTO members (email, password, firstname, lastname)
+        values ('$email','$password', 'test', 'input')";
+      if ($conn->pg_query($sql)){
+        echo "New record is inserted sucessfully";
+      }
+      else{
+        echo "Error: ". $sql ."c"; //$conn->error;
+      }
+      $conn->pg_close();
     }
+  }
+  else{
+    echo "Password should not be empty";
+    die();
+  }
 }
 else{
-    echo "Email should not be empty";
-    die();
+  echo "Email should not be empty";
+  die();
 }
 ?>
