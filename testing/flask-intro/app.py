384007e88308041641c46c8795da1c49674ef569
Kishan Tailor <Tailorkishan123@gmail.com>
2019-04-11 13:14:44 -0400

    Flask overhaul

---
+++ testing/flask-intro/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+@app.route('/login')
+def login():	
+	return render_template("Login.html")
+
+if __name__ == '__main__':
+	app.run(debug=True)	
