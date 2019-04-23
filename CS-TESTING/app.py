f03d35d076731ddc3dfd947976a476bb323f7ed1
Jarod1736 <46686094+Jarod1736@users.noreply.github.com>
2019-04-23 10:18:19 -0400

    added missing files and sorted images within static

--- CS-TESTING/app.py
+++ CS-TESTING/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 #import dj_database_url
 
-conn = psycopg2.connect("host=ec2-23-23-92-204.compute-1.amazonaws.com dbname=d1fs1cm170ct9t user=gxupvblzzfulmn password=6f218f9e00cb85e2d96043b8a25898951fd0fbd475a5bcbeb9eb2ba4cc42d072")
-cur = conn.cursor()
+#conn = psycopg2.connect("host=ec2-23-23-92-204.compute-1.amazonaws.com dbname=d1fs1cm170ct9t user=gxupvblzzfulmn password=6f218f9e00cb85e2d96043b8a25898951fd0fbd475a5bcbeb9eb2ba4cc42d072")
+#cur = conn.cursor()
 
-email = request.values.get('email')
+#email = request.values.get('email')
 
 #email = raw_input('Please enter your email: ')
 #password = raw_input('Please enter your password: ')
 #firstName = raw_input('Please enter your first name: ')
 #lastName = raw_input('Please enter your last name: ')
 
-insert_query = "INSERT INTO members VALUES('" + str(email) + "', '" + str(password) + "', '" + str(firstName) + "', '" + str(lastName) + "')"
+#insert_query = "INSERT INTO members VALUES('" + str(email) + "', '" + str(password) + "', '" + str(firstName) + "', '" + str(lastName) + "')"
 
 #cur.execute(insert_query)
 
